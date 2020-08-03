4c903ed016ae9afce3b30bd5daca51257705d32c
Inkuantum <63226427+Inkuantum@users.noreply.github.com>
2020-08-04 00:46:40 +0200

    Update 0.1.6
    -Player Movement
    -Player Animation
    -TileMap and LevelDesign

--- Game for BGJ 2020.2/Assets/Scripts/Character/PlayerMovement.cs
+++ Game for BGJ 2020.2/Assets/Scripts/Character/PlayerMovement.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
 
+    bool jump = false;
+
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        
     }
 }
