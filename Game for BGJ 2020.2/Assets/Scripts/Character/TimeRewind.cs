f5506031255614313bb159da7ed0496d2a59f078
Inkuantum <63226427+Inkuantum@users.noreply.github.com>
2020-08-04 11:58:47 +0200

    Update 0.1.7
    -Time Rewind built in
    -Smooth Player Camera Follow

---
+++ Game for BGJ 2020.2/Assets/Scripts/Character/TimeRewind.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeRewind : MonoBehaviour
+{
+    public List<GameObject> houses;
+
+    public int activeHouse = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeHouseState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Time Rewind"))
+        {
+            activeHouse++;
+            if (activeHouse >= houses.Count) activeHouse = 0;
+            ChangeHouseState();
+        }
+    }
+
+    void ChangeHouseState()
+    {
+        //Loop through houses list
+        for(int i = 0; i<houses.Count; i++)
+        {
+            GameObject currentHouse = houses[i];
+            for (int j = 0; j<currentHouse.transform.childCount; j++)
+            {
+                if(activeHouse == i)
+                {
+                    currentHouse.transform.GetChild(j).gameObject.SetActive(true);
+                }
+                else
+                {
+                    currentHouse.transform.GetChild(j).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
