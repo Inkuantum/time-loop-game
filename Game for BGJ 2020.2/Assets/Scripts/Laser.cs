5bdfffb87327f602e8decae041c91d8cec4095e7
Inkuantum <63226427+Inkuantum@users.noreply.github.com>
2020-08-07 20:30:15 +0200

    Update 0.1.11
    -Laser

---
+++ Game for BGJ 2020.2/Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.parent.childCount; i++)
+        {
+            if (transform.parent.GetChild(i).gameObject != this.gameObject)
+            {
+                if (transform.parent.GetChild(i).gameObject.name.Equals(this.gameObject.name))
+                {
+                    Debug.Log("Found Laser");
+                    Transform linkedLaser = transform.parent.GetChild(i);
+                    Transform line = transform.GetChild(0);
+                    LineRenderer lineRender = line.GetComponent<LineRenderer>();
+                    lineRender.SetPosition(1, linkedLaser.localPosition);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game for BGJ 2020.2/Assets/Scripts/Line.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LineRenderer lineRender = this.gameObject.GetComponent<LineRenderer>();
+        RaycastHit2D hit;
+
+        if(Physics2D.Raycast(lineRender.GetPosition(0), lineRender.GetPosition(1)){
+
+        }
+    }
+}
